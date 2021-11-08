ea53cdb07dd586a7cb4191a5e355bcf805615707
EugenRommel <napoleon_hewp@hotmail.com>
2021-11-08 22:19:24 +0800

    Add election code, implement Node as child of ThreadingTCPServer, read Node config from config file to init node

--- Node.py
+++ Node.py
@@ -1,5 +1,10 @@
+import argparse
+import configparser
 import logging
+import socket
 import socketserver
+
+from logging.handlers import RotatingFileHandler
 
 VOTE_REQUEST_TYPE = "VOTE_REQUEST"
 VOTE_RESPONSE_TYPE = "VOTE_RESPONSE"
@@ -69,14 +74,30 @@ class AppendEntryResponse:
                                  self.term, self.success)
 
 
-class Node(socketserver.StreamRequestHandler):
+class RpcHandler(socketserver.StreamRequestHandler):
+    def handle(self) -> None:
+        data = self.rfile.readline().strip()
+        logging.info("Received %s from %s", data, self.client_address)
+        data_str = data.decode('utf-8')
+        fields = data_str.split(":")
+        type_str = fields[0]
+        if type_str == VOTE_REQUEST_TYPE:
+            # According to TcpServer implementation, self.server
+            # in BaseRequestHandler will be instance of TcpServer.
+            # As in this case, it is Node instance.
+            self.server.process_vote_request(fields)
+
+
+class Node(socketserver.TCPServer):
     FOLLOWER = 0
     CANDIDATE = 1
     LEADER = 2
 
-    def __init__(self, request, client_address, server,
-                 role, node_id, ip, port):
-        super().__init__(request, client_address, server)
+    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True,
+                 role=FOLLOWER, node_id=0, neighbors=None):
+        self._ip, self._port = server_address
+        logging.info("server_address: %s", server_address)
+        socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate)
         self._role = role
         self._cur_term = 0
         self._id = node_id
@@ -84,15 +105,14 @@ class Node(socketserver.StreamRequestHandler):
         self._log_entries = []
         self._commit_index = 0
         self._last_applied = -1
-        self._peers = list()
-        self._ip = ip
-        self._port = port
+        self._peers = neighbors
 
-    def process_vote_request(self, msg_fields):
+    def process_vote_request(self, out_file, msg_fields):
         term_in_request = int(msg_fields[1])
         resp = VoteResponse(-1, False)
         if term_in_request < self._cur_term:
-            self.wfile.write(b'%s' % resp)
+            resp_msg = '%s' % resp
+            out_file.write(resp_msg.encode('utf-8'))
         else:
             if self._vote_for is None and \
                     (int(msg_fields[4]) > self._log_entries[-1].term or
@@ -100,15 +120,8 @@ class Node(socketserver.StreamRequestHandler):
                       int(msg_fields[3]) > len(self._log_entries) - 1)):
                 resp.granted = True
                 resp.term = int(msg_fields[4])
-                self.wfile.write(b'%s' % resp)
-
-    def handle(self) -> None:
-        data = self.rfile.readline().strip()
-        logging.info("Received %s from %s", data, self.client_address)
-        fields = data.split(b":")
-        type_str = fields[0]
-        if type_str == VOTE_REQUEST_TYPE:
-            self.process_vote_request(fields)
+                resp_msg = '%s' % resp
+                out_file.write(resp_msg.encode('utf-8'))
 
     def request_vote(self):
         last_log_index = len(self._log_entries) - 1
@@ -116,12 +129,63 @@ class Node(socketserver.StreamRequestHandler):
             last_log_term = self._log_entries[last_log_index].term
         else:
             last_log_term = 0
-        rpc_message = VoteRequest(self._cur_term, self._id,
-                                  last_log_index,
-                                  last_log_term)
+        rpc_message = '%s' % VoteRequest(self._cur_term, self._id,
+                                         last_log_index,
+                                         last_log_term)
         logging.info("Request vote: %s", rpc_message)
-        for ip, port in self._peers:
-            self.wfile.write(b'%s\n' % rpc_message)
+        granted_count = 0
+        if self._peers is None:
+            self._role = Node.LEADER
+        else:
+            for peer_ip, peer_port in self._peers:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((peer_ip, peer_port))
+                    s.sendall(rpc_message.encode('utf-8'))
+                    data = s.recv(1024)
+                    resp_msg = data.decode('utf-8')
+                    _, _, granted = resp_msg.split(":")
+                    if granted:
+                        granted_count += 1
+            if granted_count > len(self._peers) / 2:
+                # Win election, change role to leader
+                self._role = Node.LEADER
+
+    def add_peer(self, peer_ip, peer_port):
+        self._peers.append((peer_ip, peer_port))
 
     def append_log(self):
         logging.info("Append logs: %s", self._log_entries)
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--conf", help="config file path", default='config.ini')
+    # Node id is an integer. It requires there is corresponding
+    # section 'Node'+ id exists in config file specified by
+    # --conf option. For example, if node id is 0, there should
+    # be a section 'Node0' in config file
+    arg_parser.add_argument("--node", type=int, help="id of node")
+    args = arg_parser.parse_args()
+    config = configparser.ConfigParser()
+    config.read(args.conf)
+    section_name = 'Node{}'.format(args.node)
+    if section_name not in config:
+        print("Node {} config not found in {}".format(args.node, args.conf))
+        exit(-1)
+    node_ip = config[section_name]['ip'].strip()
+    node_port = int(config[section_name]['port'])
+    node_peers = config[section_name]['peers'].split(',')
+    peers = [p.split(':') for p in node_peers]
+
+    logging.basicConfig(
+        handlers=[RotatingFileHandler(
+            'raft.log', maxBytes=20 * 1024 * 1024, backupCount=10)],
+        level=logging.DEBUG,
+        format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S')
+
+    logging.info("Node run on %s:%s with id %s", node_ip, node_port, args.node)
+    logging.info("Peer nodes: %s", peers)
+    with Node((node_ip, node_port), RpcHandler, True, Node.FOLLOWER,
+              node_id=args.node, neighbors=peers) as n:
+        n.serve_forever()
